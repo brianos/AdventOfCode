1d4512d608f7fc70c21fdf3e4725ada0a1aa0327
Brian O'Sullivan <briangos@gmail.com>
2017-12-02 08:17:26 +0000

    day01 first star

---
+++ 2017/day01/day01/Processor.cs
@@ -0,0 +1,23 @@
+﻿namespace day01
+{
+    public static class Processor
+    {
+        public static int Process(string input)
+        {
+            string inputWithFirstCharacterAppendedToEnd = input + input[0];
+
+            int result = 0;
+            int lastNumber = -1;
+            foreach (char charNumber in inputWithFirstCharacterAppendedToEnd)
+            {
+                int number = int.Parse(charNumber.ToString());
+                if (number == lastNumber)
+                {
+                    result += number;
+                }
+                lastNumber = number;
+            }
+            return result;
+        }
+    }
+}
