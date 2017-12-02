669483327f0e2229fceb46c8a1f274e6f6ac460b
Brian O'Sullivan <briangos@gmail.com>
2017-12-02 09:14:20 +0000

    day02 first star

---
+++ 2017/day02/day02/ChecksumCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace day02
+{
+    public static class ChecksumCalculator
+    {
+        public static int GetChecksum(int[][] input)
+        {
+            int checksum = 0;
+            foreach (int[] row in input)
+            {
+                int min = row.Min(x => x);
+                int max = row.Max(x => x);
+                int difference = max - min;
+                checksum += difference;
+            }
+            return checksum;
+        }
+    }
+}
