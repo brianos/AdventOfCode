5f008a54646eafdad9a60c04084040cabf90af8e
Brian O'Sullivan <briangos@gmail.com>
2017-12-07 22:16:12 +0000

    day02 second star

--- 2017/day02/day02/ChecksumCalculator.cs
+++ 2017/day02/day02/ChecksumCalculator.cs
@@ -4,7 +4,7 @@ namespace day02
 {
     public static class ChecksumCalculator
     {
-        public static int GetChecksum(int[][] input)
+        public static int GetChecksumForPartA(int[][] input)
         {
             int checksum = 0;
             foreach (int[] row in input)
@@ -14,6 +14,32 @@ namespace day02
                 int difference = max - min;
                 checksum += difference;
             }
+            return checksum;
+        }
+
+        public static int GetChecksumForPartB(int[][] input)
+        {
+            int checksum = 0;
+
+            foreach (int[] row in input)
+                for (int i = 0; i < row.Length; i++)
+                    for (int j = 0; j < row.Length; j++)
+                    {
+                        if (i == j)
+                            continue;
+
+                        if (row[i] % row[j] == 0)
+                        {
+                            checksum += row[i] / row[j];
+                            i = j = row.Length + 1;
+                        }
+                        else if (row[j] % row[i] == 0)
+                        {
+                            checksum += row[j] / row[i];
+                            i = j = row.Length + 1;
+                        }
+                    }
+
             return checksum;
         }
     }
