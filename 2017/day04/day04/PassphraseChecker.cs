cf1893428b31674a0dfb033faf63bb001b1d5409
Brian O'Sullivan <briangos@gmail.com>
2017-12-08 12:56:18 +0000

    day04 first star

---
+++ 2017/day04/day04/PassphraseChecker.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace day04
+{
+    public static class PassphraseChecker
+    {
+        public static int PassphrasesValidPartA(string[] passphrases)
+        {
+            int countOfValidPassphrases = 0;
+
+            foreach (var passphrase in passphrases)
+            {
+                if (!passphrase.Split(' ').GroupBy(x => x.GetHashCode()).Any(g => g.Count() > 1))
+                {
+                    countOfValidPassphrases++;
+                }
+            }
+
+            return countOfValidPassphrases;
+        }
+    }
+}
