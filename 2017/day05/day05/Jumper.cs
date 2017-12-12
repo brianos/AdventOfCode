8840c60b839dbb7fb833cd336dcecd83770feae7
Brian O'Sullivan <briangos@gmail.com>
2017-12-12 11:09:17 +0000

    day05 first star

---
+++ 2017/day05/day05/Jumper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace day05
+{
+    public class Jumper
+    {
+        public static int ProcessorForPartA(string offsets)
+        {
+            int[] offsetArray = offsets.Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+
+            int jumpsRequired = 0;
+            int currentPosition = 0;
+
+            do
+            {
+                jumpsRequired++;
+
+                int currentOffset = offsetArray[currentPosition];
+
+                int newPosition = currentPosition + currentOffset;
+
+                if (newPosition >= offsetArray.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    currentOffset++;
+                    offsetArray[currentPosition] = currentOffset;
+                    currentPosition = newPosition;
+                }
+
+            } while (true);
+
+            return jumpsRequired;
+        }
+    }
+}
