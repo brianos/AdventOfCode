709e0f7b01e68654b5c4cebdd1f603ff90de36cc
Brian O'Sullivan <briangos@gmail.com>
2017-12-14 17:04:08 +0000

    day06 first star

---
+++ 2017/day06/day06.tests/MemoryBankReallocatorTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace day06.tests
+{
+    [TestFixture]
+    public class MemoryBankReallocatorTests
+    {
+        [Test]
+        public void TestsSuppliedForPartA()
+        {
+            var memoryBanks = new int[4] { 0, 2, 7, 0 };
+
+            var result = MemoryBankReallocator.Reallocate(memoryBanks);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void GetProblemSolutionForPartA()
+        {
+            var memoryBanks =
+                new int[16] { 2, 8, 8, 5, 4, 2, 3, 1, 5, 5, 1, 2, 15, 13, 5, 14 };
+
+            var result = MemoryBankReallocator.Reallocate(memoryBanks);
+
+            Assert.AreEqual(3156, result);
+        }
+    }
+}
---
+++ 2017/day06/day06/MemoryBankReallocator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace day06
+{
+    public class MemoryBankReallocator
+    {
+        public static int Reallocate(int[] memoryBanks)
+        {
+            var knownStates = new List<string>();
+            do
+            {
+                knownStates.Add(ConvertIntegerArrayToStringRepresentation(memoryBanks));
+
+                var maxMemoryBankSize = memoryBanks.Max();
+                var maxMemoryBankLocation = Array.IndexOf(memoryBanks, maxMemoryBankSize);
+
+                var reallocationPointer = maxMemoryBankLocation + 1 == memoryBanks.Length ? 0 : maxMemoryBankLocation + 1;
+
+                memoryBanks[maxMemoryBankLocation] = 0;
+
+                for (int i = 0; i < maxMemoryBankSize; i++)
+                {
+                    memoryBanks[reallocationPointer++]++;
+                    reallocationPointer = reallocationPointer == memoryBanks.Length ? 0 : reallocationPointer;
+                }
+            } while (!knownStates.Contains(ConvertIntegerArrayToStringRepresentation(memoryBanks)));
+
+            return knownStates.Count;
+        }
+
+        private static string ConvertIntegerArrayToStringRepresentation(int[] memoryBanks)
+        {
+            return String.Join("", memoryBanks);
+        }
+    }
+}
