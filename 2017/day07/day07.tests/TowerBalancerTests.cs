8fcb6e40a18dfc07c876be415882a4d44d45d8be
Brian O'Sullivan <briangos@gmail.com>
2017-12-21 12:53:43 +0000

    day07 second star wip

---
+++ 2017/day07/day07.tests/TowerBalancerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace day07.tests
+{
+    [TestFixture]
+    public class TowerBalancerTests
+    {
+        [Test]
+        public void TestsSuppliedForPartB()
+        {
+            var inputData = EmbeddedResourceReader.Read("day07.tests.input.TestSuppliedInputFromAdventOfCode.txt");
+            var parsedInputData = InputDataParser.Parse(inputData);
+            var result = TowerBalancer.GetBalancedWeight(parsedInputData);
+            Assert.AreEqual(60, result);
+        }
+
+        [Test]
+        public void GetProblemSolutionForPartB()
+        {
+            var inputData = EmbeddedResourceReader.Read("day07.tests.input.ProblemInputFromAdventOfCode.txt");
+            var parsedInputData = InputDataParser.Parse(inputData);
+            var result = TowerBalancer.GetBalancedWeight(parsedInputData);
+            Assert.AreEqual(-1000000, result);
+        }
+    }
+}
---
+++ 2017/day07/day07/TowerBalancer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day07
+{
+    public class TowerBalancer
+    {
+        public static int GetBalancedWeight(IEnumerable<ProgramInfo> input)
+        {
+            ProgramInfo rootElement = GetRootElement(input);
+
+            var firstLevelChildrenWithSupportingWeight = rootElement
+                                                        .Supporting
+                                                        .Select(x => new
+                                                            Tuple<string, int>(x, GetTowerWeight(input, x)));
+
+            var weightAggregate = firstLevelChildrenWithSupportingWeight
+                                .GroupBy(x => x.Item2)
+                                .Select(group => new
+                                {
+                                    Weight = group.Key,
+                                    Count = group.Count()
+                                }).ToArray();
+
+
+            var firstLevelChildWithDifferentWeightName = firstLevelChildrenWithSupportingWeight
+                                            .Where(x => x.Item2 == weightAggregate.Where(y => y.Count == 1).First().Weight)
+                                            .First();
+
+            var firstLevelChildWithDifferentWeight = GetElement(input, firstLevelChildWithDifferentWeightName.Item1);
+
+            var weightDifference = Math.Abs(weightAggregate[0].Weight - weightAggregate[1].Weight);
+
+            return Math.Abs(firstLevelChildWithDifferentWeight.Weight - weightDifference);
+        }
+
+        private static int GetTowerWeight(IEnumerable<ProgramInfo> input, string elementName)
+        {
+            var element = GetElement(input, elementName);
+
+            int weight = 0;
+
+            if (element.Supporting != null)
+            {
+                foreach (var item in element.Supporting)
+                {
+                    weight += GetTowerWeight(input, item);
+                }
+            }
+
+            return element.Weight + weight;
+        }
+
+        private static ProgramInfo GetRootElement(IEnumerable<ProgramInfo> input)
+        {
+            var rootElementName = RootElementLocator.GetRootElement(input);
+            return GetElement(input, rootElementName);
+        }
+
+        private static ProgramInfo GetElement(IEnumerable<ProgramInfo> input, string elementName)
+        {
+            return input.Where(x => x.Name == elementName).First();
+        }
+    }
+}
